09d7cd8608cc66b37a7b4c6c1edcb9634d595668
Rahmat Subandi <rhmtin12@gmail.com>
2020-08-26 15:17:25 +0700

    editing content

--- routes/mahasiswa.js
+++ routes/mahasiswa.js
@@ -10,5 +10,21 @@ router.post("/", mahasiswaController.addMahasiswa); // End point untuk tambah da
 router.put("/", mahasiswaController.editMahasiswa); // Mendeklarasikan untuk edit Mahasiswa
 router.delete("/:id", mahasiswaController.deleteMahasiswa); // membuat endpoint delete
 
+/**
+ * GET -> menampilkan seluruh data
+ * POST -> menambahkan data
+ * PUT/:id -> mengubah data
+ * DELETE/:id -> menghapus data
+ * GET/:id -> menampilkan data berdasarkan id tertentu.
+ */
+
+/**
+ * GET /api/mahasiswa Menampilkan seluruh data
+ * POST /api/mahasiswa Menambah data
+ * GET /api/mahasiswa/{id} Menampikan data berdasarkan ID
+ * PUT /api/mahasiswa/{id} Perbarui data
+ * DELETE /api/mahasiswa/{id} Hapus data
+ */
+
 // Lalu export routernya
 module.exports = router;
